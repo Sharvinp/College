08a740d42a0ec24399779d45befc72e84da90b0f
CryptoScooby <52949072+crpytoscooby@users.noreply.github.com>
2020-07-14 21:36:45 +0530

    Create 2 table.cpp

---
+++ SE Programs/DSA/2 table.cpp
@@ -0,0 +1,16 @@
+//Write a program to generate a table of 2, using do-while.
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout<<"\n2 * "<<i<<" = "<<2*i;
+        i++;
+    } while (i<=10);
+    return 0;
+    
+}

1c0f1b39535887d44f786ae6bcc0ac92b5562158
Sharvin Pharande <52949072+crpytoscooby@users.noreply.github.com>
2020-09-28 23:50:50 +0530

    little updatation

--- SE Programs/OOPL/multiple_inheritance.cpp
+++ SE Programs/OOPL/multiple_inheritance.cpp
@@ -1,48 +1,123 @@
 #include<iostream>
+
 using namespace std;
 
-class Area
+class Personal
 {
 	public:
-		int getArea(int l, int b)
-		{
-			return l * b;
-		}
+	char name[30], email[20], address[30];
+	int age;
+	void getPersonalData()
+	{
+		cin.ignore();
+		cout << "Enter name: ";
+		cin.getline(name, 30);
+		cout << "Enter age: ";
+		cin >> age;
+		cin.ignore();
+		cout << "Enter email ID: ";
+		cin.getline(email, 20);
+		cout << "Enter address: ";
+		cin.getline(address, 30);
+	}
 };
 
-class Perimeter
+class Professional
 {
 	public:
-		int getPerimeter(int l, int b)
-		{
-			return 2*(l + b);
-		}
+	int exp, salary;
+	char skills[50], designation[20];
+	void getProfessionalData()
+	{
+		cout << "Enter designation: ";
+		cin.getline(designation, 20);
+		cout << "Enter salary: ";
+		cin >> salary;
+		cout << "Enter work experience in years: ";
+		cin >> exp;
+		cin.ignore();
+		cout << "Enter professional skills: ";
+		cin.getline(skills, 50);
+	}
 };
 
-class Rectangle : public Area, public Perimeter
+class Academic
 {
-	int length;
-	int breadth;
 	public:
-		Rectangle()
-		{
-			length = 45;
-			breadth = 23;
-		}
-		int area()
-		{
-			return Area::getArea(length, breadth);
-		}
-		int perimeter()
-		{
-			return Perimeter::getPerimeter(length, breadth);
-		}
+	int passYear;
+	float cgpa;
+	char degree[10], college[20];
+	void getAcademicData()
+	{
+		cout << "Enter degree: ";
+		cin.getline(degree, 10);
+		cout << "Enter college: ";
+		cin.getline(college, 20);
+		cout << "Enter CGPA: ";
+		cin >> cgpa;
+		cout << "Enter year of passing: ";
+		cin >> passYear;
+	}
+};
+
+class Biodata: public Personal, public Professional, public Academic
+{
+	public:
+	void createBiodata()
+	{
+		cout << "\nPERSONAL DETAILS\nName: " << name;
+		cout << "\nAge: " << age;
+		cout << "\nEmail ID: " << email;
+		cout << "\nAddress: " << address << endl;
+		
+		cout << "\nACADEMIC DETAILS\nDegree: " << degree;
+		cout << "\nCollege: " << college;
+		cout << "\nCGPA: " << cgpa;
+		cout << "\nYear of passing: " << passYear << endl;
+		
+		cout << "\nPROFESSIONAL DETAILS\nDesignation: " << designation;
+		cout << "\nSalary: " << salary;
+		cout << "\nWork experience: " << exp << " years";
+		cout << "\nProfessional skills: " << skills << endl;
+	}
 };
 
 int main()
 {
-	Rectangle rt;
-	cout << "Area : " << rt.area() << endl;
-	cout << "Perimeter : " << rt.perimeter() << endl;
+	Biodata b;
+	int choice;
+	char repeat;
+	
+	do
+	{
+		cout << "\n1. Enter details\n2. Create biodata\nEnter your choice: ";
+		cin >> choice;
+		
+		switch(choice)
+		{
+			case 1:
+				{
+					b.getPersonalData();
+					b.getProfessionalData();
+					b.getAcademicData();
+					break;
+				}
+			case 2:
+				{
+					b.createBiodata();
+					break;
+				}
+			default:
+				{
+					cout << "\nInvalid Input!";
+					break;
+				}
+		}
+		
+		cout << "\nDo you want to perform another operation? (y/n)\n";
+		cin >> repeat;
+	}
+	while(repeat=='y' || repeat=='Y');
+	
 	return 0;
 }
